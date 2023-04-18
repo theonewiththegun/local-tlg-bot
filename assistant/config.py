e22c5cbee3f0b7b209fe85cedfced04cab7a64c7
Igor Popkov <popkov97.97.97@gmail.com>
2023-04-18 19:40:47 +0300

    Fixed a couple of bugs

--- assistant/config.py
+++ assistant/config.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-with open(os.environ['SETTINGS_JSON']) as handle:
+with open(os.environ['SECRETS_JSON']) as handle:
     SECRETS = json.loads(handle.read())
 
 TLG_BOT_TOKEN = SECRETS['TLG_BOT_TOKEN']
